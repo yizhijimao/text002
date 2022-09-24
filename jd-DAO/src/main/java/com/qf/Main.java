bd82489cf10911772ad40140ecca47a79155b019
yizhijimao <xtaymnmn@gmail.com>
2022-09-24 19:24:55 +0900

    修改了DAO中的main

--- jd-DAO/src/main/java/com/qf/Main.java
+++ jd-DAO/src/main/java/com/qf/Main.java
@@ -2,6 +2,7 @@ package com.qf;
 
 public class Main {
     int daidai = 110;
+    String daidai1 = "1987";
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
